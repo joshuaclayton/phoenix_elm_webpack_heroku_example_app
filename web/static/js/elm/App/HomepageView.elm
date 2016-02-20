9465b6b28df65aae7987d6fe5bb5e16965c2ea13
Joshua Clayton <jclayton@thoughtbot.com>
2016-02-19 22:10:55 -0500

    Include GitHub link from within the application

--- web/static/js/elm/App/HomepageView.elm
+++ web/static/js/elm/App/HomepageView.elm
@@ -9,5 +9,7 @@ homepage =
     section [class "welcome"]
     [ h1 [] [text "Phoenix + Elm + Webpack on Heroku"]
     , h2 [] [text "or, all the fun things"]
+    , p  [] [text "This is a Phoenix application running Elm on Heroku."]
+    , a  [href "https://github.com/joshuaclayton/phoenix_elm_webpack_heroku_example_app"] [text "View the source"]
     ]
   ]
